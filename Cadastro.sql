b0a3006c9717372f8b426d5f89d5915735427549
Ykyy <kaykylima48@gmail.com>
2022-10-15 22:20:49 -0300

    Add files via upload

--- Cadastro.sql
+++ Cadastro.sql
@@ -3,9 +3,12 @@ Drop table login;
 Create table login(
 
 User_id     int    Primary key not null ,
-Email       VARCHAR2(50) not null ,
 User_name   VARCHAR2(50) not null ,
+Email       VARCHAR2(50) not null ,
+CEP         NUMBER(8)    NOT NULL ,
+Endereco    VARCHAR2(50) NOT NULL ,
 Senha       VARCHAR2(32) not null 
 );
 
 
+select*from login;
